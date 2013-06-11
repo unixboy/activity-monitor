61de36c91910c483847a5a1daa22f4adbb899cbf
cmichi <mich@elmueller.net>
2013-06-11 18:18:48 +0200

    initial cronjob files

---
+++ 4-hrs.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+# this file gets called by a cronjob each 12 hours
+
+#./check-for-changes.sh \
+#	"alice@mail.de" \
+#	"http://some-resource.net/index.html" \
+
+# or with preprocessing of content:
+#./check-for-changes.sh \
+#	"alice@mail.de" \
+#	"http://some-resource.net/index.html" \
+#	"preprocess/only-body.sh"
